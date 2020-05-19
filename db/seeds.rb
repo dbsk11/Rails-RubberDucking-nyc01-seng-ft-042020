a9531bc73600b42e04bf7d6b516a23e22384c532
Jessica <github email address>
2020-05-19 17:38:34 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+puts "...destroying student data"
+Student.destroy_all
+
+puts "...destroying duck data"
+Duck.destroy_all
+
+puts "...creating students"
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
+puts "...creating ducks"
 5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed, student_id: Student.all.sample.id)
 end
+
+
